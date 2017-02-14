45cd2c538585e0537b4a6b4d62db7a8bf5de8976
Kevin Delabroye <kevin_delabroye@ens.univ-artois.fr>
2017-02-14 10:44:07 +0100

    correction bug son ubunutu

--- src/Run/Sound.java
+++ src/Run/Sound.java
@@ -2,42 +2,35 @@ package Run;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
-import javax.sound.sampled.*;
-
 public class Sound {
-	public static ArrayList<AudioClip> audio=new ArrayList<AudioClip>();
-	
-	public static Sound accueil=new Sound("/Ressource/son/audioAccueil.wav");
-	public static Sound menu=new Sound("/Ressource/son/audioMenu.wav");
-	public static Sound game=new Sound("/Ressource/son/audioJeu.wav");
-	
-	private int indice=0;
-	
-	public Sound(String fichier){
-		try{
+	public static ArrayList<AudioClip> audio = new ArrayList<AudioClip>();
+
+	public static final Sound accueil = new Sound("/Ressource/Son/audioAccueil.wav");
+	public static final Sound menu = new Sound("/Ressource/Son/audioMenu.wav");
+	public static final Sound game = new Sound("/Ressource/Son/audioJeu.wav");
+
+	private int indice = 0;
+
+	public Sound(String fichier) {
+		try {
+			System.out.println(Sound.class.getResource(fichier));
 			audio.add(Applet.newAudioClip(Sound.class.getResource(fichier)));
 			indice++;
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void loop(int i){
-		try{
-			new Thread(){
-				public void run(){
+
+	public static void loop(int i) {
+		try {
+			new Thread() {
+				public void run() {
 					audio.get(i).loop();
 				}
-			}.start();	
-		}
-		catch(Exception e){
+			}.start();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
